1e9b2e7be80180e0bcf23da46ff88d81750ccf6b
lehaha42 <146331806+lehaha42@users.noreply.github.com>
2025-03-29 10:25:55 +1000

    laba6 -> laba7 laba5 -> laba6 new laba5

--- finch_labs/src/main/laba5/Main.java
+++ finch_labs/src/main/laba5/Main.java
@@ -16,22 +16,7 @@ public class Main {
         System.out.println("Program finished");
     }
 
-    public static void run(Finch f) {
+    public static void run(Finch f){
 
-        double[][] arr = {
-                {50., 180.},
-                {50., 180.}
-        };
-        double speed = 99.;
-        int runs = 2;
-
-        f.setBeak(100, 100, 100);
-        for (int i = 0; i < runs; i++){
-            for (double[] data : arr) {
-                f.setMove("F", data[0], speed);
-                f.setTurn("L", data[1], speed);
-            }
-        }
-        f.setBeak(0, 0, 0);
     }
 }
---
+++ finch_labs/src/main/laba7/Main.java
@@ -0,0 +1,71 @@
+package main.laba7;
+
+
+import main.finch.Finch;
+
+import java.util.Arrays;
+
+class Main{
+    public static void main(String[] args) {
+        System.out.println("Program started");
+
+        Finch f = new Finch();
+
+        run(f);
+
+        f.stopAll();
+        f.disconnect();
+
+        System.out.println("Program finished");
+    }
+
+    public static void run(Finch f){
+        int N = 100;
+        double diff = 0.0, sq_diff = 0.0;
+        f.setBeak(100, 100, 100);
+        for (int i = 0; i < N; i++){
+            double[] acc = f.getAcceleration();
+            double a = acc[0] * acc[0] + acc[1] * acc[1] + acc[2] * acc[2];
+            sq_diff += a;
+            diff += Math.sqrt(a);
+            f.pause(1.0/N);
+        }
+        System.out.println("Diff: "+diff/N+"   Square Diff: "+Math.sqrt(sq_diff/N));
+        System.out.println("start");
+        while(!(f.getButton("A") || f.getButton("B"))){
+            double[] acc = f.getAcceleration();
+            double a = Math.sqrt(acc[0] * acc[0] + acc[1] * acc[1] + acc[2] * acc[2]);
+            System.out.println(Arrays.toString(acc));
+            if(a > diff/N+0.5){
+                f.setBeak(100, 0, 0);
+            }else{
+                f.setBeak(0, 100, 0);
+            }
+            f.pause(0.01);
+        }
+    }
+
+    public static void run2(Finch f){
+        int N = 100;
+        double diff = 0.0, sq_diff = 0.0;
+        f.setBeak(100, 100, 100);
+        for (int i = 0; i < N; i++){
+            double[] acc = f.getAcceleration();
+            double a = acc[0] * acc[0] + acc[1] * acc[1] + acc[2] * acc[2];
+            sq_diff += a;
+            diff += Math.sqrt(a);
+            f.pause(1.0/N);
+        }
+        System.out.println("Diff: "+diff/N+"   Square Diff: "+Math.sqrt(sq_diff/N));
+        System.out.println("start");
+        while(!(f.getButton("A") || f.getButton("B"))){
+            double[] acc = f.getAcceleration();
+            f.setBeak(50+(int)bound(acc[0], -10, 10)*5, 50+(int)bound(acc[1], -10, 10)*5, 50+(int)bound(acc[2], -10, 10)*5);
+            f.pause(0.01);
+        }
+    }
+
+    public static double bound(double n, double a, double b){
+        return Math.min(Math.max(n, a), b);
+    }
+}
