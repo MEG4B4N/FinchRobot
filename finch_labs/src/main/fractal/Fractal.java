0d333b59a8d46c2f733f60deef7c8ddf9ee73566
MEG4B4N <samsungvl3005@gmail.com>
2025-04-10 12:46:15 +1000

    fractal

--- finch_labs/src/main/fractal/Fractal.java
+++ finch_labs/src/main/fractal/Fractal.java
@@ -20,7 +20,25 @@ public class Fractal {
     }
 
     public static void hilbertFractal(Finch f) {
-        f_x(f, 3);
+        f_x(f, 4);
+    }
+
+    public static void fx(Finch f, int d) {
+        f.setMove("F", dist, speed);
+        if (d <= 0) return;;
+        fx(f, d - 1);
+        f.setTurn("L", 90, speed);
+        fy(f, d - 1);
+        f.setTurn("L", 90, speed);
+    }
+
+    public static void fy(Finch f, int d) {
+        f.setMove("F", dist, speed);
+        if (d <= 0) return;
+        f.setTurn("L", -90, speed);
+        fx(f, d - 1);
+        f.setTurn("L", -90, speed);
+        fy(f, d - 1);
     }
 
     public static void f_x(Finch f, int d){
